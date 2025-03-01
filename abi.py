b3984d68aae61816aa906773c933d30c00a02d8d
Anjalaidevi2005 <anjalaidevisubramani@gmail.com>
2025-03-01 15:59:24 +0530

    Initial commit

---
+++ abi.py
@@ -0,0 +1,67 @@
+# x=int(input())
+# if(x%2==0):
+#     print("Even")
+# else:
+#     print("odd")
+  
+# y=5
+# if(y%2!=0):
+#     print("odd")
+# else:
+#     print("even") 
+# a=56
+# b=76
+# c=92
+
+# a,b,c=c,a,b
+# print(a,b,c)
+
+# s="hello world"[::-1]
+# print(s)
+
+# x=list("School")
+# print(x)
+
+# c=int(input("enter the celius"))
+# f=(c*9/5)*32
+# print(f)
+
+# a=(35)
+# print(type(a))
+# b=(10.3)
+# print(type(b))
+# c=("college")
+# print(type(c))
+# d=["file"]
+# print(type(d))
+
+# K="villupuram"[::-1]
+# print(K)
+
+
+# a="school"
+# b="college"
+# c=a,b
+# print(c)
+
+
+# a="100"
+# b="25"
+# c='10.5'
+# a=int(a)
+# b=int(b)
+# c=float(c)
+# sum=a+b
+# sub=sum-c
+# output=str(sub)+"is the answer"
+# print(output)
+
+# s="at school"[::-1]
+# print(s)
+# t="at school"
+# print(t)
+# if =t:
+#     print("s and t is equal is palindrome")
+# else:
+#     print("s and t is not equal is not palindrome")
+
---
+++ ps.py
@@ -0,0 +1,77 @@
+#a=int(input("enter a first number:"))
+#b=int(input("enter a second number:"))
+#c=a+b
+#print(c)
+
+# a=int(input("enter a number"))
+# if (a%2)==0:
+#    print("this number is even number:")
+# else:
+#     print("this number is add number")
+#
+# 
+#abi=["apple","orange","banana","kivi"]
+#for i in abi:
+#    if i=="banana":
+#     continue
+#    print(i)
+
+#n=int(input("enter a number:"))
+#for i in range(n+1):
+#      print(i*"*")
+
+# x=5
+# x="abi"
+# print(x)
+
+# x=5
+# y="abi"
+#  print(type(x))
+#  print(type(y))
+# my_var="anu"
+# myvamyvar="abi"
+# r="at"
+# print(myvar)
+# print(my_var)
+# print(my_var)
+
+# a="10"
+# b="20"
+# c=a+b
+# print(c)
+# name=input()
+# age=input()
+# print("my name is:",name)
+# print("my age is:",age)
+
+
+
+# a="ragamaliga"
+# print(a.upper())
+
+# thislist = ["apple", "banana", "cherry"]
+# i = 0
+# while i < len(thislist):
+#   print(thislist[i])
+#   i = i + 1
+
+
+
+# thislist = ["apple", "banana", "cherry"]
+# i = 0
+# print(len(thislist))
+# while i < len(thislist):
+#   print(thislist[i])
+#   i = i + 1
+def oddeven(n):
+    #n=int(input("enter a number"))
+    if n%2==0:
+        print("even")
+    else:
+        print("odd")
+def my_function(a,b):
+    c=a+b
+    return c
+m=int(input("Enter number1"))
+n=int(input("Enter number2"))
+print(my_function(m,n))
